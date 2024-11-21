49758450ab79f14975609b1488896df637a59417
Alberto Lima <alberto.btlima@outlook.com>
2024-11-20 21:10:58 -0300

    add: Episodio

--- 01-trabalhando-com-lambdas-streams-e-spring-framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ 01-trabalhando-com-lambdas-streams-e-spring-framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,14 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
   private final Scanner leitura = new Scanner(System.in);
@@ -34,6 +35,31 @@ public class Principal {
       DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
       temporadas.add(dadosTemporada);
     }
+//  temporadas.forEach(t -> System.out.println(t));
     temporadas.forEach(System.out::println);
+
+
+
+//    for (int i = 0; i < dadosSerie.totalTemporadas(); i++) {
+//      List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//      for (int j = 0; j < episodiosTemporada.size(); j++) {
+//        System.out.println(episodiosTemporada.get(j).titulo());
+//      }
+//    }
+
+    temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+    List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+      .flatMap(t -> t.episodios().stream())
+      .toList();
+
+    System.out.println("\nTop 5 episódios");
+    dadosEpisodios.stream()
+      .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+      .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+      .limit(5)
+      .forEach(System.out::println);
+
+    List<Episodio> episodios =
   }
 }
