d7014c6b3d59369644293fe0f2f90d4b77b40825
Alberto Lima <alberto.btlima@outlook.com>
2024-11-26 19:36:53 -0300

    busca por nome

--- 02-TabelaFipe/src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ 02-TabelaFipe/src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -2,16 +2,19 @@ package br.com.alura.TabelaFipe.principal;
 
 import br.com.alura.TabelaFipe.model.Dados;
 import br.com.alura.TabelaFipe.model.Modelos;
+import br.com.alura.TabelaFipe.model.Veiculo;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
 import br.com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-  private Scanner leitura = new Scanner(System.in);
-  private ConsumoApi consumoApi = new ConsumoApi();
-  private ConverteDados converteDados = new ConverteDados();
+  private final Scanner leitura = new Scanner(System.in);
+  private final ConsumoApi consumoApi = new ConsumoApi();
+  private final ConverteDados converteDados = new ConverteDados();
 
   private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
@@ -60,5 +63,33 @@ public class Principal {
     modeloLista.modelos().stream()
       .sorted(Comparator.comparing(Dados::nome))
       .forEach(System.out::println);
+
+    System.out.println("\nDigite o nome do veículo a ser buscado: ");
+    var nomeVeiculo = leitura.nextLine();
+
+    List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+      .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+      .toList();
+
+    System.out.println("\nModelos filtrados: ");
+    modelosFiltrados.forEach(System.out::println);
+
+    System.out.println("Digite o código do veículo: ");
+    var codigoModelo = leitura.nextLine();
+
+    endereco = endereco + "/" + codigoModelo + "/anos";
+    json = consumoApi.obterDados(endereco);
+    List<Dados> anos = converteDados.obterLista(json, Dados.class);
+    List<Veiculo> veiculos = new ArrayList<>();
+
+    for (Dados ano : anos) {
+      var enderecoAnos = endereco + "/" + ano.codigo();
+      json = consumoApi.obterDados(enderecoAnos);
+      Veiculo veiculo = converteDados.obterDados(json, Veiculo.class);
+      veiculos.add(veiculo);
+    }
+
+    System.out.println("\nTodos os veículos filtrados com avaliações por ano: ");
+    veiculos.forEach(System.out::println);
   }
 }
